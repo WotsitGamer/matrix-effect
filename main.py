de50ea1642750201f0efd1fa65e3753fdd13827b
X0AM <watsonjude8@gmail.com>
2022-03-27 15:47:04 +0100

    added essential features

---
+++ main.py
@@ -0,0 +1,20 @@
+from multiprocessing.connection import wait
+from random import randint
+
+
+import random
+from time import sleep
+import os
+os.system('color 0a')
+
+chars = ["a", "b", "c", "1", "0", ".", "-", "_", "*", "/", "+"]
+
+
+def RandomChars():
+    randomChar = chars[(random.randint(0,10))]
+    return randomChar
+
+while True:
+    print(RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),RandomChars(),)
+    sleep(0.01)
+    os.system('color 0a')
